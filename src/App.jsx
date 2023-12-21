fc2969799f39c36c951c8113ff1bbbab017d0272
nhungnguyen-9 <nguyennhung9846@gmail.com>
2023-12-21 18:06:32 +0700

    add dark-light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import Button from '@mui/material/Button'
 import AddAlarmIcon from '@mui/icons-material/AddAlarm'
 
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
 function App() {
 
   return (
     <>
+      <ModeToggle />
       <div>Nhung Nguyen</div>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
