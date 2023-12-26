21a842e1bba189c76921a18940a8b6ff6b33ecae
nhungnguyen-9 <nguyennhung9846@gmail.com>
2023-12-26 19:44:46 +0700

    add app bar

--- src/theme.js
+++ src/theme.js
@@ -1,23 +1,87 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
-import { deepOrange, orange, cyan, teal } from '@mui/material/colors'
+import { orange, blue, grey } from '@mui/material/colors'
 
 // Create a theme instance
 const theme = extendTheme({
     trello: {
-        appBarHeight: '48px',
+        appBarHeight: '54px',
         boardBarHeight: '58px'
     },
     colorSchemes: {
         light: {
             palette: {
-                primary: teal,
-                secondary: deepOrange
+                primary: {
+                    main: blue[500],
+                    light: 'hsl(215,90%,32.7%)'
+                },
+                secondary: {
+                    main: '#fff'
+                }
             }
         },
         dark: {
             palette: {
-                primary: cyan,
-                secondary: orange
+                primary: {
+                    main: blue[500],
+                    light: grey[900]
+                },
+                secondary: grey
+            }
+        }
+    },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: {
+                body: {
+                    '*::-webkit-scrollbar': {
+                        width: '6px',
+                        height: '6px'
+                    },
+                    '*::-webkit-scrollbar-thumb': {
+                        backgroundColor: '#bdc3c7',
+                        borderRadius: '8px'
+                    },
+                    '*::-webkit-scrollbar-thumb:hover': {
+                        backgroundColor: '#18dcff'
+                    }
+                }
+            }
+        },
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    textTransform: 'none',
+                    color: 'white'
+                }
+            }
+        },
+        MuiInputLabel: {
+            styleOverrides: {
+                root: ({ theme }) => ({
+                    color: theme.palette.secondary.main,
+                    fontSize: '0.875rem'
+                })
+            }
+        },
+        MuiOutlinedInput: {
+            styleOverrides: {
+                root: ({ theme }) => ({
+                    color: theme.palette.secondary.main,
+                    fontSize: '0.875rem',
+                    '.MuiOutlinedInput-notchedOutline': {
+                        borderColor: 'white',
+                        backgroundColor: 'rgba(255, 255, 255, 0.2)'
+                    },
+                    '&:hover': {
+                        '.MuiOutlinedInput-notchedOutline': {
+                            backgroundColor: 'rgba(255, 255, 255, 0.4)',
+                            borderColor: 'white'
+                        }
+                    },
+                    '& fieldset': {
+                        borderWidth: '0.5px !important'
+                    }
+                })
             }
         }
     }
