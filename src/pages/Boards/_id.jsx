2733a3e540a3636b59eea54917d91d098745c027
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-02-06 12:28:18 +0700

    aggregate

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -2,7 +2,7 @@ import Container from '@mui/material/Container'
 import BoardBar from './BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import AppBar from '~/components/AppBar/AppBar'
-import { mockData } from '~/api/mock-data'
+// import { mockData } from '~/api/mock-data'
 import { useEffect, useState } from 'react'
 import { fetchBoardDetailsAPI } from '~/api'
 
@@ -11,7 +11,7 @@ function Board() {
     const [board, setBoard] = useState(null)
 
     useEffect(() => {
-        const boardId = '65bc75c6aca06cc87bdb9d1e'
+        const boardId = '65c1b3e2f844de21cd0b60ed'
         //Call API
         fetchBoardDetailsAPI(boardId).then(board => {
             setBoard(board)
