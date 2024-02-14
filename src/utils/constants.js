0fa86415dbf4421f9af31427622d51be0e0fe058
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-02-14 22:30:30 +0700

    fix BUILD_MODE

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,12 @@
 // chua bien tinh
-export const API_ROOT = 'https://trello-api-s3z0.onrender.com'
+let apiRoot = ''
+
+if (process.env.BUILD_MODE === 'dev') {
+    apiRoot = 'https://localhost:8017'
+}
+
+if (process.env.BUILD_MODE === 'production') {
+    apiRoot = 'https://trello-api-s3z0.onrender.com'
+}
+
+export const API_ROOT = apiRoot
